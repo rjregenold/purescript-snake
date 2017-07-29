3552f4d8a9597d5762af803d887a1eb594c68dd4
RJ Regenold <rjregenold@gmail.com>
2017-07-29 09:03:40 -0600

    uses game state.

--- src/Main.purs
+++ src/Main.purs
@@ -6,7 +6,7 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Eff.Random (RANDOM, randomInt)
 import Control.Monad.Eff.Timer (TIMER)
-import Data.Array (length, replicate, snoc, tail, (:), (!!))
+import Data.Array (deleteAt, length, null, replicate, snoc, tail, (:), (!!), (..))
 import Data.Foldable (for_)
 import Data.Int (floor, toNumber)
 import Data.Maybe (Maybe(..), fromMaybe, maybe)
@@ -67,6 +67,7 @@ type GameState =
   , input    :: ActiveInput
   , player   :: Player
   , apple    :: Apple
+  , eating   :: Boolean
   }
 
 type AppEff eff =
@@ -95,8 +96,8 @@ wrap mn mx v | v < mn    = mx
              | v > mx    = mn
              | otherwise = v
 
-playerLogic :: ActiveInput -> Player -> Player
-playerLogic input p = (wrapPlayer <<< velocity <<< trimTrail <<< addTrail <<< move input) p
+playerLogic :: GameState -> Player
+playerLogic st = (wrapPlayer <<< velocity <<< trimTrail <<< addTrail <<< eat st.eating <<< move st.input) st.player
   where
     move None p  = p
     move Left p  = p { vel = { x: -1.0, y: 0.0 } }
@@ -104,18 +105,21 @@ playerLogic input p = (wrapPlayer <<< velocity <<< trimTrail <<< addTrail <<< mo
     move Right p = p { vel = { x: 1.0, y: 0.0 } }
     move Down p  = p { vel = { x: 0.0, y: 1.0 } }
 
-    -- TODO: do not hardcode board size here
-    wrapPlayer p = p { pos = { x: wrap 0.0 19.0 p.pos.x, y: wrap 0.0 19.0 p.pos.y } }
+    eat e p =
+      if e
+      then p { len = p.len + 1 }
+      else p
 
-    addTrail :: Player -> Player
     addTrail p = p { trail = snoc p.trail p.pos }
 
-    trimTrail :: Player -> Player
     trimTrail p | length p.trail > p.len = p { trail = fromMaybe [] (tail p.trail) }
                 | otherwise              = p
 
-player :: Signal ActiveInput -> Signal Player
-player input = foldp playerLogic initialPlayer (sampleOn frameRate input)
+    -- TODO: do not hardcode board size here
+    wrapPlayer p = p { pos = { x: wrap 0.0 19.0 p.pos.x, y: wrap 0.0 19.0 p.pos.y } }
+
+player :: GameState -> GameState
+player st = st { player = playerLogic st }
 
 getApplePos :: Apple -> Maybe Position
 getApplePos a = a.pool !! a.idx
@@ -127,18 +131,11 @@ nextApplePos a =
       then a { idx = 0 }
       else a { idx = idx' }
 
-appleLogic :: Player -> Apple -> Apple
-appleLogic p a = 
-  case getApplePos a of
-    Just pos -> if intersects p.pos pos
-                then nextApplePos a
-                else a
-    Nothing  -> a
-
-apple :: Array Position -> Signal Player -> Signal Apple
-apple pool = foldp appleLogic initialApple
-  where
-    initialApple = { pool: pool, idx: 0 }
+apple :: GameState -> GameState
+apple st = 
+  if st.eating
+  then st { apple = nextApplePos st.apple }
+  else st
 
 input :: forall eff. Eff (dom :: DOM | eff) (Signal ActiveInput)
 input = do
@@ -159,11 +156,11 @@ input = do
     toInput _ _ _ true = Down
     toInput _ _ _ _    = None
 
-render :: forall eff. Context2D -> (Tuple Player Apple) -> Eff (canvas :: CANVAS | eff) Unit
-render ctx (Tuple p a) = do
+render :: forall e. Context2D -> GameState -> Eff (canvas :: CANVAS | e) Unit
+render ctx st = do
   drawBoard ctx
-  drawApple ctx a
-  drawPlayer ctx p
+  drawApple ctx st.apple
+  drawPlayer ctx st.player
   where
     drawBoard ctx = do
       void $ setFillStyle "black" ctx
@@ -188,117 +185,60 @@ randPos mn mx = do
   y <- randomInt mn mx
   pure { x: toNumber x, y: toNumber y }
 
+allPos :: Array Position
+allPos = do
+  x <- 0..19
+  y <- 0..19
+  pure { x: toNumber x, y: toNumber y }
+
+shuffle :: forall a e. Array a -> Eff (random :: RANDOM | e) (Array a)
+shuffle = go <<< Tuple []
+  where
+    go (Tuple acc xs) =
+      if null xs
+      then pure acc
+      else do
+        idx <- randomInt 0 (length xs - 1)
+        go (fromMaybe (Tuple [] []) (f acc <$> xs !! idx <*> deleteAt idx xs))
+
+    f acc x = Tuple (snoc acc x)
+
+eating :: GameState -> GameState
+eating st = st { eating = maybe false (intersects st.player.pos) (getApplePos st.apple) }
+
+updateInput :: ActiveInput -> GameState -> GameState
+updateInput input st = st { input = input }
+
+gameLogic :: ActiveInput -> GameState -> GameState
+gameLogic input = (player <<< apple <<< eating <<< updateInput input)
+
+gameState :: forall e. Dimensions -> Eff (dom :: DOM, random :: RANDOM | e) (Signal GameState)
+gameState dim = do
+  activeInput <- input
+  {--pool <- sequence (replicate 400 (randPos 0 19))--}
+  pool <- shuffle allPos
+  pure (foldp gameLogic (initialGameState pool) (sampleOn frameRate activeInput))
+  where
+    initialGameState pool =
+      { boardDim: dim
+      , input: None
+      , player: initialPlayer
+      , apple: { pool: pool
+               , idx: 0 
+               } 
+      , eating: false
+      }
+
 runGame :: forall eff. CanvasElement -> Eff (AppEff eff) Unit
 runGame canvas = do
   log "starting game"
   dim <- getCanvasDimensions canvas
   ctx <- getContext2D canvas
-  activeInput <- input
-  randPool <- sequence (replicate 400 (randPos 0 19))
-  let player' = player activeInput
-      apple'  = apple randPool player'
-      scene   = Tuple <~ player' ~ apple'
-  runSignal (scene ~> render ctx)
+  game <- gameState dim
+  runSignal (game ~> render ctx)
 
 main :: forall eff. Eff (AppEff eff) Unit
 main = do
   mCanvas <- getCanvasElementById "app"
   log "in main"
   maybe (log "missing canvas") runGame mCanvas
-
-
-
-
-
-
-
-
-
-
-
-{--runGame :: forall eff. CanvasElement -> Eff (AppEff eff) Unit--}
-{--runGame canvas = do--}
-  {--log "in game"--}
-  {--dim <- getCanvasDimensions canvas--}
-  {--ctx <- getContext2D canvas--}
-  {--let st = {boardDim: dim, player: initialPlayer, apple: {x: 15.0, y: 15.0}, stepNum: 0}--}
-  {--inputs <- mkInputs--}
-  {--let frames = every second--}
-  {--let game = foldp gameLogic (pure st) frames -- (sampleOn frames inputs)--}
-  {--runSignal (render ctx <$> game)--}
-
-  {--where--}
-    {--mkInputs = do--}
-      {--leftInputs <- keyPressed leftKeyCode--}
-      {--upInputs <- keyPressed upKeyCode--}
-      {--rightInputs <- keyPressed rightKeyCode--}
-      {--downInputs <- keyPressed downKeyCode--}
-      {--pure ({ left: _, up: _, right: _, down: _ } <$> leftInputs <*> upInputs <*> rightInputs <*> downInputs)--}
-
-    {--gameLogic :: forall eff. Inputs -> Eff (random :: RANDOM | eff) GameState -> Eff (random :: RANDOM | eff) GameState--}
-    {--gameLogic inputs gameState = do--}
-      {--st <- appleLogic =<< gameState--}
-      {--pure (st { stepNum = st.stepNum + 1, player = playerLogic inputs st.player })--}
-
-    {--appleLogic :: forall eff. GameState -> Eff (random :: RANDOM, console :: CONSOLE | eff) GameState--}
-    {--appleLogic st = do--}
-      {--ifM (isEatingApple st.apple st.player)--}
-        {--(newApplePos >>= (\pos -> pure (st { player = st.player { len = st.player.len + 1 }, apple = pos })))--}
-        {--(pure st)--}
-
-    {--isEatingApple :: forall eff. Position -> Player -> Eff (console :: CONSOLE | eff) Boolean--}
-    {--isEatingApple apple player = do--}
-      {--[>log ("apple: " <> (show apple.x) <> ", " <> (show apple.y))<]--}
-      {--[>log ("player: " <> (show player.pos.x) <> ", " <> (show player.pos.y))<]--}
-      {--pure (apple.x == player.pos.x && apple.y == player.pos.y)--}
-
-    {--newApplePos :: forall eff. Eff (random :: RANDOM, console :: CONSOLE | eff) Position--}
-    {--newApplePos = do--}
-      {--[>log "getting new apple pos"<]--}
-      {--newX <- randomInt 0 (floor (tileCount - 1.0))--}
-      {--newY <- randomInt 0 (floor (tileCount - 1.0))--}
-      {--pure { x: toNumber newX, y: toNumber newY }--}
-
-    {--playerLogic :: Inputs -> Player -> Player--}
-    {--playerLogic inputs = trimTrail <<< updateTrail <<< updatePlayerPosition <<< move inputs.left inputs.up inputs.right inputs.down--}
-
-    {--move true _ _ _ p = p { vel = {x: -1.0, y: 0.0} }--}
-    {--move _ true _ _ p = p { vel = {x: 0.0, y: -1.0} }--}
-    {--move _ _ true _ p = p { vel = {x: 1.0, y: 0.0} }--}
-    {--move _ _ _ true p = p { vel = {x: 0.0, y: 1.0} }--}
-    {--move _ _ _ _ p = p--}
-
-    {--updatePlayerPosition p = p { pos = clampPlayerPosition { x: p.pos.x + p.vel.x, y: p.pos.y + p.vel.y } }--}
-
-    {--clampPlayerPosition :: Position -> Position--}
-    {--clampPlayerPosition pos | pos.x < 0.0        = pos { x = tileCount }--}
-                            {--| pos.x >= tileCount = pos { x = 0.0 }--}
-                            {--| pos.y < 0.0        = pos { y = tileCount }--}
-                            {--| pos.y >= tileCount = pos { y = 0.0 }--}
-                            {--| otherwise          = pos--}
-
-    {--updateTrail :: Player -> Player--}
-    {--updateTrail p = p { trail = snoc p.trail p.pos }--}
-
-    {--trimTrail :: Player -> Player--}
-    {--trimTrail p | length p.trail > p.len = p { trail = fromMaybe [] (tail p.trail) }--}
-                {--| otherwise              = p--}
-
-    {--render ctx st = do--}
-      {--st' <- st--}
-      {--drawBoard ctx st'--}
-      {--drawApple ctx st'--}
-      {--drawPlayer ctx st'--}
-
-    {--drawBoard ctx st = do--}
-      {--ctx' <- setFillStyle "black" ctx--}
-      {--void $ fillRect ctx' {x: 0.0, y: 0.0, w: st.boardDim.width, h: st.boardDim.height}--}
-
-    {--drawApple ctx st = do--}
-      {--ctx' <- setFillStyle "red" ctx--}
-      {--void $ fillRect ctx' {x: st.apple.x * gridSize, y: st.apple.y * gridSize, w: gridSize - 2.0, h: gridSize - 2.0}--}
-
-    {--drawPlayer ctx st = do--}
-      {--ctx' <- setFillStyle "lime" ctx--}
-      {--for_ st.player.trail $ \pos -> do--}
-        {--void $ fillRect ctx' {x: pos.x * gridSize, y: pos.y * gridSize, w: gridSize - 2.0, h: gridSize - 2.0}--}
