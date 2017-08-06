7a4f8f3b8c65d9af44039ef5fdff12b646907787
RJ Regenold <rjregenold@gmail.com>
2017-08-06 12:00:23 -0500

    cleans up rendering.

--- src/Main.purs
+++ src/Main.purs
@@ -12,7 +12,8 @@ import Data.Int (toNumber)
 import Data.Maybe (Maybe(..), fromMaybe, maybe)
 import Data.Tuple (Tuple(..))
 import DOM (DOM)
-import Graphics.Canvas (CANVAS, CanvasElement, Context2D, Dimensions, fillRect, getCanvasDimensions, getCanvasElementById, getContext2D, setFillStyle)
+import Graphics.Canvas (CANVAS, CanvasElement, Context2D, Dimensions, fillRect, fillText, getCanvasDimensions, getCanvasElementById
+                       , getContext2D, measureText, setFillStyle, setFont)
 import Signal (Signal, foldp, runSignal, sampleOn, (~>))
 import Signal.DOM (keyPressed)
 import Signal.Time (every, second)
@@ -49,7 +50,7 @@ type Player =
 
 initialPlayer :: Player
 initialPlayer =
-  { pos: Position { x: 10, y: 10 }
+  { pos: Position { x: (tileCount / 2), y: (tileCount / 2) }
   , vel: Velocity { x: 0, y: 0 }
   , dir: Nothing
   , len: 3
@@ -103,28 +104,34 @@ type AppEff eff =
   | eff
   )
 
+-- | Defines the frame rate signal.
 frameRate :: Signal Number
 frameRate = every (second / 15.0)
 
+-- | Applies velocity to a position.
 velocity :: Position -> Velocity -> Position
 velocity (Position p) (Velocity v) = Position { x: p.x + v.x, y: p.y + v.y }
 
+-- | Tests for an intersection between two points.
 intersects :: Position -> Position -> Boolean
 intersects (Position a) (Position b) = a.x == b.x && a.y == b.y
 
+-- | Ensures the given value is inside the bounds.
 wrap :: Int -> Int -> Int -> Int
 wrap mn mx v | v < mn    = mx
              | v >= mx   = mn
              | otherwise = v
 
+-- | Given bounds, ensures the given position stays within them.
 wrapPos :: Int -> Int -> Int -> Int -> Position -> Position
 wrapPos mnx mxx mny mxy (Position p) = 
   Position { x: wrap mnx mxx p.x, y: wrap mny mxy p.y }
 
+-- | Gets the next player step from the current game state.
 playerLogic :: GameState -> Player
 playerLogic st = (wrapPlayer <<< playerVelocity <<< trimTrail <<< addTrail <<< eat st.eating <<< move st.input) st.player
   where
-    -- move checks the previous position to ensure the player does not
+    -- move checks the previous direction to ensure the player does not
     -- turn back onto themselves (thus ending the game)
     move None p  = p
 
@@ -154,6 +161,7 @@ playerLogic st = (wrapPlayer <<< playerVelocity <<< trimTrail <<< addTrail <<< e
 
     wrapPlayer p = p { pos = wrapPos 0 tileCount 0 tileCount p.pos }
 
+-- | Handles the player logic (including checks for game over).
 player :: GameState -> GameState
 player = checkGameOver <<< updatePlayer
   where
@@ -217,48 +225,71 @@ activeInput = do
 -- | Renders the game state.
 render :: forall e. Context2D -> GameState -> Eff (canvas :: CANVAS | e) Unit
 render ctx st = do
-  drawBoard ctx
-  drawApple ctx st.apple
-  drawPlayer ctx st.player
+  renderGame
+  renderOverlay
   where
-    drawBoard ctx' = do
-      void $ setFillStyle "black" ctx'
-      void $ fillRect ctx' { x: 0.0
-                           , y: 0.0
-                           , w: st.boardDim.width
-                           , h: st.boardDim.height 
-                           }
+    renderGame = do
+      renderBoard
+      renderApple st.apple
+      renderPlayer st.player
 
-    drawApple ctx' a = 
+    renderBoard = do
+      void $ setFillStyle "black" ctx
+      void $ fillRect ctx { x: 0.0
+                          , y: 0.0
+                          , w: st.boardDim.width
+                          , h: st.boardDim.height 
+                          }
+
+    renderApple a =
       case getApplePos a of
-        Just (Position pos) -> do
-          void $ setFillStyle "red" ctx'
-          void $ fillRect ctx' { x: (toNumber pos.x) * gridSize
-                               , y: (toNumber pos.y) * gridSize
-                               , w: pieceSize
-                               , h: pieceSize
-                               }
+        Just pos -> renderTile "red" pos
         Nothing  -> pure unit
 
-    drawPlayer ctx' p@{ pos: (Position curPos) } = do
-      void $ setFillStyle "#00cc00" ctx'
-      for_ p.trail $ \(Position pos) -> do
-        void $ fillRect ctx' { x: (toNumber pos.x) * gridSize
-                             , y: (toNumber pos.y) * gridSize
-                             , w: pieceSize
-                             , h: pieceSize
-                             }
-      void $ setFillStyle playerHeadColor ctx'
-      void $ fillRect ctx' { x: (toNumber curPos.x) * gridSize
-                           , y: (toNumber curPos.y) * gridSize
-                           , w: pieceSize
-                           , h: pieceSize
-                           }
+    renderPlayer p = do
+      renderTrail p.trail
+      renderHead p.pos
+
+    renderTrail ts = for_ ts (renderTile playerTrailColor)
+
+    renderHead = renderTile playerHeadColor
+
+    renderTile color (Position pos) = do
+      void $ setFillStyle color ctx
+      void $ fillRect ctx { x: (toNumber pos.x) * gridSize
+                          , y: (toNumber pos.y) * gridSize
+                          , w: pieceSize
+                          , h: pieceSize
+                          }
+
+    playerTrailColor = "#00cc00"
 
     playerHeadColor =
       if st.playState == GameOver
       then "#ffa500"
       else "#00ff00"
+
+    renderOverlay = 
+      case st.playState of
+        NotStarted -> renderNotStarted
+        GameOver   -> renderGameOver
+        _          -> pure unit
+
+    renderNotStarted = 
+      let msg = "Press the arrow keys to start"
+      in do
+        void $ setFont "20px sans-serif" ctx
+        m <- measureText ctx msg
+        void $ setFillStyle "white" ctx
+        void $ fillText ctx msg ((st.boardDim.width - m.width) / 2.0) 40.0
+
+    renderGameOver = 
+      let msg = "GAME OVER :("
+      in do
+        void $ setFont "20px sans-serif" ctx
+        m <- measureText ctx msg
+        void $ setFillStyle "white" ctx
+        void $ fillText ctx msg ((st.boardDim.width - m.width) / 2.0) 40.0
 
 -- | Returns a collection of all possible game positions.
 allPos :: Array Position
